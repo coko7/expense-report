a48486048eb85fd8b21caecb736f5f51a42364ad
Coko <91132775+Coko7@users.noreply.github.com>
2026-03-02 16:39:52 +0100

    fix: enforce python version to 3.11.8 in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,24 @@
 #!/usr/bin/env bash
 
-[[ ! -d 'data' ]] && mkdir data
+PYTHON_BIN="$HOME/.pyenv/versions/3.11.8/bin/python"
 
-python -m venv '.invoice2data-venv' \
-    && source .invoice2data-venv/bin/activate \
-    && pip install --upgrade setuptools \
-    && pip install invoice2data \
-    && pip install openpyxl
+current_version=$($PYTHON_BIN --version)
+expected_version='Python 3.11.8'
+
+if [[ "$current_version" != "$expected_version" ]]; then
+    echo "❌ Incorrect Python version:"
+    echo "- Expected: $expected_version"
+    echo "- Got: $current_version"
+    exit 1
+fi
+
+[[ -d 'data' ]] || mkdir data
+
+VENV_NAME='.invoice2data-venv'
+
+rm --recursive --force "$VENV_NAME"
+$PYTHON_BIN -m venv "$VENV_NAME"
+source "$VENV_NAME/bin/activate"
+
+# pip install --upgrade pip setuptools wheel
+pip install invoice2data openpyxl
